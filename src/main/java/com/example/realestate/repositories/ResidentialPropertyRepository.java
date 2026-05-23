4e0fafd71f2d3ae759b0ba769303310fcde7bb2b
it25103918 <it25103918@my.sliit.lk>
2026-05-24 00:25:47 +0530

    Delete unwanted code

--- src/main/java/com/example/realestate/repositories/ResidentialPropertyRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.realestate.repositories;
-
-import com.example.realestate.models.ResidentialProperty;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ResidentialPropertyRepository extends JpaRepository<ResidentialProperty, Long> {
-}
